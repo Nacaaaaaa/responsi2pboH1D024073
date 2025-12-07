5ed934fad82e7b1e16168df09fbf9c18e6a443b7
Nalendra Wicaksana <nalendraw123@gmail.com>
2025-12-07 17:19:55 +0700

    Update Customer.java

--- Responsi-2/pert4/Customer.java
+++ Responsi-2/pert4/Customer.java
@@ -1,9 +1,15 @@
 class Customer {
-    // TODO: Buatkan atribut
+    String nama;
+    String noID;
+    int totalBelanja;
 
-    // TODO: Sediakan constructor
+    Customer(String nama, String noID, int totalBelanja) {
+        this.nama = nama;
+        this.noID = noID;
+        this.totalBelanja = totalBelanja;
+    }
 
     void tampilkanInfo() {
-        // TODO: tampilkan data customer
+        System.out.println("Nama: " + nama + " | ID: " + noID + " | Total Belanja: Rp " + totalBelanja);
     }
 }
