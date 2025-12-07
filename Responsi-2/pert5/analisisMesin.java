15c700600648b9181468614b72b39396a7a24730
Nalendra Wicaksana <nalendraw123@gmail.com>
2025-12-07 17:57:50 +0700

    Update analisisMesin.java

--- Responsi-2/pert5/analisisMesin.java
+++ Responsi-2/pert5/analisisMesin.java
@@ -1,30 +1,59 @@
 class analisisMesin {
     public static void main(String[] args) {
-
-        // Buat array untuk menyimpan berbagai jenis mesin
-
-        // Isi array dengan objek mesinMotor
-
-        // Isi array dengan objek mesinTraktor
-
-        // Isi array dengan objek mesinTraktorListrik
+        defaultMesin[] daftarMesin = new defaultMesin[5];
+        daftarMesin[0] = new mesinMotor("Honda Supra X", 125, "Bebek");
+        daftarMesin[1] = new mesinTraktor("Kubota MX5200", 520, 5.0);
+        daftarMesin[2] = new mesinTraktorListrik("EcoTrac Z900", 300, 4.2, 70.0);
+        daftarMesin[3] = new mesinMotor("Yamaha R25", 250, "Sport");
+        daftarMesin[4] = new mesinTraktorListrik("Volta FarmX", 200, 3.5, 80.0);
 
         System.out.println("=== DATA MESIN MEGATECH ===");
-
-        // Loop untuk menampilkan info masing-masing mesin
-
-        // Loop untuk menampilkan kategori dan performa
+        for (defaultMesin mesin : daftarMesin) {
+            mesin.tampilInfo();
+            System.out.println("Kategori: " + mesin.kategoriMesin());
+            System.out.printf("Performa: %.1f%n%n", mesin.nilaiPerforma());
+        }
 
         System.out.println("=== SUARA MESIN ===");
-
-        // Loop untuk menghasilkan suara tiap mesin (instanceof)
+        for (defaultMesin mesin : daftarMesin) {
+            if (mesin instanceof mesinMotor) {
+                ((mesinMotor) mesin).suaraMesin();
+            } else if (mesin instanceof mesinTraktorListrik) {
+                // Pengecekan dilakukan dari subclass paling spesifik (Traktor Listrik)
+                ((mesinTraktorListrik) mesin).suaraMesin();
+            } else if (mesin instanceof mesinTraktor) {
+                ((mesinTraktor) mesin).suaraMesin();
+            }
+        }
 
         System.out.println("\n=== MESIN PERFORMA TERTINGGI ===");
+        defaultMesin mesinTertinggi = daftarMesin[0];
+        double performaTertinggi = mesinTertinggi.nilaiPerforma();
 
-        // Logika menemukan mesin dengan performa tertinggi
+        for (int i = 1; i < daftarMesin.length; i++) {
+            if (daftarMesin[i].nilaiPerforma() > performaTertinggi) {
+                performaTertinggi = daftarMesin[i].nilaiPerforma();
+                mesinTertinggi = daftarMesin[i];
+            }
+        }
+        System.out.printf("%s -> %.1f%n", mesinTertinggi.namaMesin, performaTertinggi);
 
         System.out.println("\n=== TOP 3 MESIN TERBAIK ===");
+        int n = daftarMesin.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                // Urutkan descending (berdasarkan performa)
+                if (daftarMesin[j].nilaiPerforma() < daftarMesin[j + 1].nilaiPerforma()) {
+                    // Tukar posisi
+                    defaultMesin temp = daftarMesin[j];
+                    daftarMesin[j] = daftarMesin[j + 1];
+                    daftarMesin[j + 1] = temp;
+                }
+            }
+        }
 
-        // Logika sorting 3 performa tertinggi
+        for (int i = 0; i < 3 && i < daftarMesin.length; i++) {
+            System.out.printf("%d. %s -> %.1f%n", (i + 1), daftarMesin[i].namaMesin, daftarMesin[i].nilaiPerforma());
+        }
     }
 }
